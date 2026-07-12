e2ffb986913558ef211210ede8fcb4928968ff70
Ashishdupargude <dupargudeashish1234@gmail.com>
2026-07-12 12:11:45 +0530

    API - Create and delete article workflow

--- tests/api/apis.spec.ts
+++ tests/api/apis.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "@playwright/test"
+import { expect, request, test } from "@playwright/test"
 
 test('FirstApi', async ({ request }) => {
 
@@ -23,6 +23,79 @@ test('FirstApi', async ({ request }) => {
 
     //check very first tag of the array data
     expect(tagResponseJSON.tags[0]).toBe('Test')
+
+})
+
+//FirstLogin run login API
+//capture the token from the login API response
+//use that token while calling the create artical API
+test('API - Create and delete article workflow', async ({ request }) => {
+
+    const tokenRespnse = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
+
+        data: {
+            "user": {
+                "email": "Panda@gmail.com",
+                "password": "Panda@08"
+            }
+        }
+    })
+    const tokenRespnseJSON = await tokenRespnse.json()
+    //console.log(tokenRespnseJSON)
+
+
+    //const authtoken = tokenRespnseJSON.user.token
+
+    const { token: authtoken } = tokenRespnseJSON.user  //Destructuring syntax
+    console.log(authtoken)
+
+    const newArticleCreate = await request.post('https://conduit-api.bondaracademy.com/api/articles/', {
+
+        data: {
+            "article": {
+                "title": "asdf2",
+                "description": "asdf",
+                "body": "asdf",
+                "tagList": [
+                    "asdf"
+                ]
+            }
+        },
+        headers: {
+            authorization: `Token ${authtoken}`
+        }
+
+
+    })
+
+    const newArticleRespnseJSON = await newArticleCreate.json()
+    console.log(tokenRespnseJSON)
+    expect(newArticleCreate.status()).toBe(201)
+    expect(newArticleRespnseJSON.article.title).toBe('asdf2')
+    const slug = newArticleRespnseJSON.article.slug
+
+
+    const articlesResponse = await request.get('https://conduit-api.bondaracademy.com/api/articles?limit=10&offset=0', {
+        headers: {
+            authorization: `Token ${authtoken}`
+        }
+    })
+
+    const articlesResponseJSON = await articlesResponse.json()
+    //console.log(articlesResponseJSON)
+    expect(articlesResponse.status()).toBe(200)
+    expect(articlesResponseJSON.articles[0].title).toBe('asdf2')
+
+
+
+    const deleteArticle = await request.delete(`https://conduit-api.bondaracademy.com/api/articles/${slug}`, {
+        headers: {
+            authorization: `Token ${authtoken}`
+        }
+
+    })
+    //204 no content
+    expect(deleteArticle.status()).toBe(204)
 
 })
 
