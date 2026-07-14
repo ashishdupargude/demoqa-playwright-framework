7ad14dd20ad681049acb6ebed41909943b99f49e
Ashishdupargude <dupargudeashish1234@gmail.com>
2026-07-14 10:10:10 +0530

    UpdatedParamTest

--- tests/paramTest1.spec.ts
+++ tests/paramTest1.spec.ts
@@ -2,18 +2,25 @@ import { expect, test } from '@playwright/test'
 
 const searchItems: string[] = ['laptop', 'Gift card', 'smartphone', 'monitor']
 
+test.describe('search functionality tests',() => {
+
+    test.beforeEach(async ({page}) => { 
+
+       await page.goto('https://demowebshop.tricentis.com/')
+    })
+
+
 for (const item of searchItems) {
 
-    test(`search, ${item}`, async ({ page }) => {
-
-
-        await page.goto('https://demowebshop.tricentis.com/')
+    test(`Search, ${item}`, async ({ page }) => {
+        
 
         // await page.getByRole('textbox', {name:'search store'}).fill(item)  it wiill not work bcoz there is is no html accesible name called <label>
         await page.locator('#small-searchterms').fill(item)
         await page.getByRole('button', { name: 'search' }).click()
 
-await expect(page.locator('.product-title').nth(0)).toContainText(item, { ignoreCase: true})        
+        await expect(page.locator('.product-title').nth(0)).toContainText(item, { ignoreCase: true })
 
     })
 }
+})
