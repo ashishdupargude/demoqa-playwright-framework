a5ec6c390f200a87c4fcc81dd38b7b70e616a47b
Ashishdupargude <dupargudeashish1234@gmail.com>
2026-07-15 08:55:06 +0530

    paramTest3Added

---
+++ test-data/data.js
@@ -0,0 +1,28 @@
+//Name export
+export const loginIDs = [
+
+    {
+        email: 'laura.taylor1234@example.com',
+        password: 'test123',
+        validity: 'valid'
+
+    },
+    {
+        email: 'abc1@example.com',
+        password: 'pass1',
+        validity: 'invalid'
+
+    },
+    {
+        email: 'abc2@example.com',
+        password: 'pass2',
+        validity: 'invalid'
+
+    },
+    {
+        email: '',
+        password: '',
+        validity: 'invalid'
+
+    }
+]
