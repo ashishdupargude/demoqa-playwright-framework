c5067fd4030172efbcf707f61ce2b010e9807ccf
Ashishdupargude <dupargudeashish1234@gmail.com>
2026-07-24 15:55:52 +0530

    HandleNewPageTC

---
+++ tests/HandleNewPage.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from '@playwright/test'
+
+test('HandelNewTab', async ({ context }) => {
+
+
+    const page = await context.newPage();
+    await page.goto('https://testpages.eviltester.com/pages/navigation/windows-names/')
+    await expect(page).toHaveTitle('Windows Links Test Page | Test Pages')
+
+    const pagePromis = context.waitForEvent('page')
+
+    await page.getByRole('link', { name: 'Window with name in new tab' }).click()
+
+    const newPage = await pagePromis
+    //await expect(newPage).toHaveTitle('Linked Page with Window Name | Test Pages');
+    await newPage.locator('#window-name-button').click()
+    await expect(newPage).toHaveTitle('Linked Page with Window Name | Test Pages');
+
+    await page.pause()
+
+
+})
--- tests/keyboard.spec.ts
+++ tests/keyboard.spec.ts
@@ -3,6 +3,11 @@ test('keyboard action', async ({page}) => {
 
     await page.goto('https://gotranscript.com/text-compare')
 
+    // const  iMac = process.platform === 'darwin'
+    //const  iMac = process.platform === 'win32'
+    // const modifierkey = iMac ? 'Meta' : 'Control'
+
+
     await page.getByPlaceholder('Paste one version of the text here.').fill('ashish')
     await page.keyboard.press('Control+A')
     await page.keyboard.press('Control+C')
@@ -10,6 +15,8 @@ test('keyboard action', async ({page}) => {
     // await page.keyboard.up('Tab')
     await page.keyboard.press('Tab')
     await page.keyboard.press('Control+V')
+
+    await expect(page.locator('[name="text2"]')).toHaveValue('ashish')
     await page.pause()
 
 
